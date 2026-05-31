8731c0644a68518b97ba83357bd38224bb2099ec
Awaji-mitop Gilbert <successgilli@gmail.com>
2026-05-31 21:11:27 +0200

    flask_wtf,flashes,flask_bootstrap: flask extentions

--- projects/flasky/app.py
+++ projects/flasky/app.py
@@ -1,20 +1,31 @@
-from flask import Flask, make_response, render_template
+from flask import Flask, make_response, render_template,redirect,url_for,session,flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap
+
+class NameForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    submit = SubmitField('submit')
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'my very secret flask app key'
 
-def index() -> str:
-    return render_template('index.html')
+Bootstrap(app)
 
-app.add_url_rule('/', 'index', index)
+@app.route('/', methods=['GET', 'POST'])
+def index()-> str:
+    nameForm = NameForm()
 
-@app.get('/user/<name>')
-def username(name: str) -> tuple[str, int]:
-    return render_template('user.html', name=name)
+    if nameForm.validate_on_submit():
+        if nameForm.name.data:
+            flash('Name change successful!', 'success')
+            session['name'] = nameForm.name.data
+            
+            return redirect(url_for('index'))
 
-@app.errorhandler(404)
-def app_error_404(e):
-    return make_response(render_template('404.html', error=e), 404)
+    sessionName = session.get('name')
+    greetName = sessionName if sessionName else 'Stranger'
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    return make_response(render_template('form.html', form=nameForm, name=greetName), 200)
