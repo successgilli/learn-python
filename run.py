eda3938096be09478a54928450221609235f18cc
Awaji-mitop Gilbert <successgilli@gmail.com>
2026-06-06 23:13:10 +0200

    readme

--- run.py
+++ run.py
@@ -1,29 +1,4 @@
-class MyManager():
-    '''
-    My manager haha
-    '''
-    def __init__(self, startCount: int):
-        self.startCount = startCount
-        self.items = [startCount]
-    
-    def __enter__(self):
-        print('entering')
-        return self
-
-    def __exit__(self, exc_type, exc, tb) -> bool:
-        print('exiting')
-        print(self.items)
-        return True # Decides if errors are propagated should they happen during resource usage
-    
-    def do(self, item):
-        print(f'doing {item}')
-        self.items.append(item)
-
-items = [284,4,2,5,3]
-
-with MyManager(3) as resource:
-    for item in items:
-        resource.do(item)
-    
-    raise Exception('bla')
-
+try:
+    raise Exception('haha')
+except Exception as e:
+    print(e.args[0])
